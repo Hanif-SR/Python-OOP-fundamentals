d3f58fb3c7943f29c15079d6933782967844b4e2
Hanif Sebastian Ramanda <118244791+Hanif-SR@users.noreply.github.com>
2025-05-22 12:02:16 +0700

    OOP example.py

--- OOP example.py
+++ OOP example.py
@@ -4,71 +4,158 @@ os.system('cls')
 
 
 class BankAccount:
-    bank_name = "TrustBank"
+    Bank_name   =   "RBA"
+    def __init__(self, name, PIN, money):
+        self.name       =   name
+        self.__PIN      =   PIN
+        self._money     =   money
+    
+    def __logic__(self, other, op):
+        if not isinstance(other, BankAccount):
+            raise ValueError("value is not an object/instance, cannot continue")
+        if op == 'eq':
+            return self.value == other.value
+        elif op == 'lt':
+            return self.value < other.value
+        elif op == 'gt':
+            return self.value > other.value
+        elif op == 'add':
+            return self.value + other.value
+        elif op == 'sub':
+            return self.value - other.value
+    def __eq__(self, other):
+        return self.__logic__(other,'eq')
+    def __lt__(self,other):
+        return self.__logic__(other,'lt')
+    def __gt__(self,other):
+        return self.__logic__(other,'gt')
+    def __add__(self,other):
+        return self.__logic__(other,'add')
+    def __sub__(self,other):
+        return self.__logic__(other,'sub')
+    def __str__(self):
+        return f"{self.name}, {self.__PIN}, {self._money}"
+    def __repr__(self):
+        return f"BankAccount(\"{self.name}\", \"{self.__PIN}\", {self._money})"
+    
+    @classmethod
+    def acc_stat(cls):
+        if cls==BankAccount:
+            return f"Your account is a standard account." 
+        elif cls==Premium:
+            return f"Your account is a premium account."
+        
+    @staticmethod
+    def acc_fee(value):
+        # transaction fee, deposit fee, withdrawal fee, etc.
+        # fee is 50
+        value = value - 50
+        return value
+    
+    def validation(self, PIN, amount):
+        if not self.__PIN==PIN:
+            raise ValueError("wrong PIN")
+        if not type(self.__PIN)==int:
+            raise ValueError("PIN must be integers")
+        if not len(str(self.__PIN))==6:
+            raise ValueError("PIN must be 6 digit")
+        if amount < 0:
+            raise ValueError("insufficient amount")
+        return True
 
-    def __init__(self, name, balance):
-        self.name = name # normal attribute
-        self._balance = balance # protected attribute
-        self.__transaction_id = 2000  # private attribute
+    def deposit(self, PIN, amount):
+        self.validation(PIN, amount)
+        if amount > 5000:
+            raise ValueError("must be a premium account!")
+        self._money += amount
+        self._money = BankAccount.acc_fee(self._money)
+        print(f"your current amount is {self._money}$")
+        return f"money deposited successfully."
+    def withdraw(self, PIN, amount):
+        self.validation(PIN, amount)
+        if amount > 5000:
+            raise ValueError("must be a premium account!")
+        if self._money < amount:
+            raise ValueError("insufficient amount!")
+        self._money -= amount
+        self._money = BankAccount.acc_fee(self._money)
+        print(f"successfully withdrawed {amount}")
+        return f"money withdrawed successfully"
+    def send_money(self, other, PIN, amount):
+        self.validation(PIN, amount)
+        if amount > 5000:
+            raise ValueError("must be a premium account!")
+        if self._money < amount:
+            raise ValueError("insufficient amount!")
+        self._money -= amount
+        other._money += amount
+        self._money = BankAccount.acc_fee(self._money)
+        self.validation(PIN, self._money)
+        return f"transaction successfull"
+    def money_check(self, PIN):
+        self.validation(PIN, self._money)
+        print(f"Name: {self.name}")
+        print(f"Your current balance is {self._money}$")
+        return f"Thank you for using our services."
+
+class Premium(BankAccount):
+    def __init__(self, name, PIN, money):
+        super().__init__(name, PIN, money)
 
     @classmethod
-    def change_bankname(cls, new_name):
-        cls.bank_name = new_name
-        return f"The bank name has been changed into {cls.bank_name}"
+    def acc_stat(cls):
+        return super().acc_stat()
     
-    @staticmethod # for example id must be 4 digit
-    def id_check(id):
-        if len(id) != 4:
-            return f"transaction id must be 4 digit."
-        elif len(id) == 4:
-            return f"you transaction id is valid"
+    def deposit(self, PIN, amount):
+        self.validation(PIN, amount)
+        self._money += amount
+        self._money = BankAccount.acc_fee(self._money)
+        print(f"your current amount is {self._money}$")
+        return f"money deposited successfully."
+    def withdraw(self, PIN, amount):
+        self.validation(PIN, amount)
+        if self._money < amount:
+            raise ValueError("insufficient amount!")
+        self._money -= amount
+        self._money = BankAccount.acc_fee(self._money)
+        print(f"successfully withdrawed {amount}")
+        return f"money withdrawed successfully"
+    def send_money(self, other, PIN, amount):
+        self.validation(PIN, amount)
+        if self._money < amount:
+            raise ValueError("insufficient amount!")
+        self._money -= amount
+        other._money += amount
+        self._money = BankAccount.acc_fee(self._money)
+        self.validation(PIN, self._money)
+        return f"transaction successfull"
+    def money_check(self, PIN):
+        self.validation(PIN, self._money)
+        print(f"Name: {self.name}")
+        print(f"You are a premium member.")
+        print(f"Your current balance is {self._money}$")
+        return f"Thank you soo much for using our services!!"
     
-    def __verify_transaction(self, given_id):  # private method
-        return given_id == self.__transaction_id
+acc1 = BankAccount("Peter",123456,1000)
+acc2 = BankAccount("Lukas",121212,2000)
 
-    def show_balance(self):
-        return f"{self.name}'s balance is ${self._balance}"
+acc3 = Premium("Bartholomew",242424,30000)
+acc4 = Premium("Jeffreyson",363636,20000)
 
-    def deposit(self, amount, transaction_id):
-        # 👈 You need to call the private method here
-        if not self.__verify_transaction(transaction_id):
-            return ValueError("incorrect transaction ID, cannot continue")
-        self._balance += amount
-        return f"{self.name} balance is now ${self._balance} after deposit with ${amount} amount"
-
-    def withdraw(self, amount, transaction_id):
-        # 👈 Only allow if balance is enough
-        if not self.__verify_transaction(transaction_id):
-            return ValueError("incorrect transaction ID, cannot continue")
-        if (self._balance < amount):
-            return f"insufficient funds"
-        self._balance -= amount
-        print(f"{self.name} is withdrawing ${amount} amount of money")
-        return f"this {self.name}'s current balance ${self._balance}"
-
-    def transfer(self, to_account, amount, transaction_id):
-        # 👈 Withdraw from self, deposit into to_account
-        if not self.__verify_transaction(transaction_id):
-            return ValueError("incorrect transaction ID, cannot continue")
-        if (self._balance < amount):
-            return f"insufficient funds"
-        self._balance -= amount
-        print(f"{self.name}'s balance is now {self._balance}")
-        to_account._balance += amount
-        print(f"transaction succeded")
-        return f"{to_account.name}'s balance is now ${to_account._balance}"
-    
-    def __str__(self):
-        return f"{self.name}'s balance is currently ${self._balance}"
-
-
-acc1 = BankAccount("Alex", 1000)
-acc2 = BankAccount("Peter", 3000)
-acc3 = BankAccount("Mike", 1500)
-
-print(acc2.show_balance())
-print(acc1)
-print(acc1.bank_name)
-print(acc1.deposit(500,2000))
-print(acc1.withdraw(500,2000))
-print(acc2.transfer(acc1,1500,2000))
+print(acc1.deposit(123456,1500))
+print('\n')
+print(acc1.send_money(acc2,123456,1000))
+print('\n')
+print(acc1.withdraw(123456,400))
+print('\n')
+print(acc1.money_check(123456))
+print(15*'-',"social gap",15*'-')
+print(acc3.deposit(242424,6500))
+print('\n')
+print(acc3.send_money(acc4,242424,7500))
+print('\n')
+print(acc3.withdraw(242424,6000))
+print('\n')
+print(acc3.money_check(242424))
+print('\n')
+print(acc4.money_check(363636))
